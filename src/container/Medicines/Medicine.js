d8c7f682675f17791fbb51edf10bcd39005be090
Bhargav1112 <bhargavbhakhar1112@gmail.com>
2022-06-27 15:59:33 +0530

    actions added to data table

--- src/container/Medicines/Medicine.js
+++ src/container/Medicines/Medicine.js
@@ -7,142 +7,145 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import * as yup from "yup";
 import { useFormik } from "formik";
-import MedicineData from "./MedicineData";
+import MedicineDataTable from "./MedicineData";
 
 function Medicine(props) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const insertToLocalStorage = (values, action) => {
+    const localData = JSON.parse(localStorage.getItem("medicine"));
+    let id = Math.floor(Math.random() * 1000 + 1);
+    let data = {
+      id,
+      ...values,
     };
+    if (!localData) {
+      localStorage.setItem("medicine", JSON.stringify([data]));
+    } else {
+      localData.push(data);
+      localStorage.setItem("medicine", JSON.stringify(localData));
+    }
+    dataMethod();
+    handleClose();
+    action.resetForm();
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const schema = yup.object().shape({
+    name: yup.string().required("Please enter name"),
+    price: yup.number().positive().required("Required"),
+    expiry: yup.string().required("Please enter expiry date"),
+    quantity: yup.number().positive().integer().required("Required"),
+  });
 
-    const insertToLocalStorage = (data, action) => {
-        const localData = JSON.parse(localStorage.getItem("medicine"));
-        if (!localData) {
-            localStorage.setItem("medicine", JSON.stringify([data]));
-        } else {
-            localData.push(data);
-            localStorage.setItem("medicine", JSON.stringify(localData));
-        }
-        dataMethod();
-        handleClose();
-        action.resetForm();
-    };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      price: "",
+      expiry: "",
+      quantity: "",
+    },
+    validationSchema: schema,
+    onSubmit: insertToLocalStorage,
+  });
 
-    const schema = yup.object().shape({
-        name: yup.string().required("Please enter name"),
-        price: yup.number().positive().required("Required"),
-        expiry: yup.string().required("Please enter expiry date"),
-        quantity: yup.number().positive().integer().required("Required"),
-    });
+  const { handleBlur, handleChange, handleSubmit, values, errors, touched } =
+    formik;
 
-    const formik = useFormik({
-        initialValues: {
-            name: "",
-            price: "",
-            expiry: "",
-            quantity: "",
-        },
-        validationSchema: schema,
-        onSubmit: insertToLocalStorage,
-    });
+  let dataMethod;
+  const onDisplayData = (method) => {
+    dataMethod = method;
+  };
 
-    const { handleBlur, handleChange, handleSubmit, values, errors, touched } =
-        formik;
-
-    let dataMethod;
-    const onDisplayData = (method) => {
-        dataMethod = method;
-    };
-
-    return (
-        <div>
-            <h1>MEDICINES.</h1>
-            <div>
-                <Button variant="contained" onClick={handleClickOpen}>
-                    Add +
-                </Button>
-                <Dialog fullWidth open={open} onClose={handleClose}>
-                    <DialogTitle>Add new Medicine</DialogTitle>
-                    <form onSubmit={handleSubmit}>
-                        <DialogContent>
-                            <TextField
-                                margin="dense"
-                                id="name"
-                                label="Medicine name"
-                                type="text"
-                                fullWidth
-                                variant="standard"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.name}
-                                name="name"
-                            />
-                            {errors.name && touched.name && (
-                                <p style={{ color: "red" }}>{errors.name}</p>
-                            )}
-                            <TextField
-                                name="price"
-                                margin="dense"
-                                id="price"
-                                label="Price"
-                                type="text"
-                                fullWidth
-                                variant="standard"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.price}
-                            />
-                            {errors.price && touched.price && (
-                                <p style={{ color: "red" }}>{errors.price}</p>
-                            )}
-                            <TextField
-                                name="quantity"
-                                margin="dense"
-                                id="quantity"
-                                label="Quantity"
-                                type="text"
-                                fullWidth
-                                variant="standard"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.quantity}
-                            />
-                            {errors.quantity && touched.quantity && (
-                                <p style={{ color: "red" }}>
-                                    {errors.quantity}
-                                </p>
-                            )}
-                            <TextField
-                                name="expiry"
-                                margin="dense"
-                                id="expiry"
-                                label="Expiry date"
-                                type="text"
-                                fullWidth
-                                variant="standard"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.expiry}
-                            />
-                            {errors.expiry && touched.expiry && (
-                                <p style={{ color: "red" }}>{errors.expiry}</p>
-                            )}
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleClose}>Cancel</Button>
-                            <Button type="submit">Add</Button>
-                        </DialogActions>
-                    </form>
-                </Dialog>
-                <MedicineData onAdd={onDisplayData} />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>MEDICINES.</h1>
+      <div>
+        <Button variant="contained" onClick={handleClickOpen}>
+          Add +
+        </Button>
+        <Dialog fullWidth open={open} onClose={handleClose}>
+          <DialogTitle>Add new Medicine</DialogTitle>
+          <form onSubmit={handleSubmit}>
+            <DialogContent>
+              <TextField
+                margin="dense"
+                id="name"
+                label="Medicine name"
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+                name="name"
+              />
+              {errors.name && touched.name && (
+                <p style={{ color: "red" }}>{errors.name}</p>
+              )}
+              <TextField
+                name="price"
+                margin="dense"
+                id="price"
+                label="Price"
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.price}
+              />
+              {errors.price && touched.price && (
+                <p style={{ color: "red" }}>{errors.price}</p>
+              )}
+              <TextField
+                name="quantity"
+                margin="dense"
+                id="quantity"
+                label="Quantity"
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.quantity}
+              />
+              {errors.quantity && touched.quantity && (
+                <p style={{ color: "red" }}>{errors.quantity}</p>
+              )}
+              <TextField
+                name="expiry"
+                margin="dense"
+                id="expiry"
+                label="Expiry date"
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.expiry}
+              />
+              {errors.expiry && touched.expiry && (
+                <p style={{ color: "red" }}>{errors.expiry}</p>
+              )}
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button type="submit">Add</Button>
+            </DialogActions>
+          </form>
+        </Dialog>
+        <MedicineDataTable onAdd={onDisplayData} />
+      </div>
+    </div>
+  );
 }
 
 export default Medicine;
--- src/container/Medicines/MedicineData.js
+++ src/container/Medicines/MedicineData.js
@@ -1,50 +1,58 @@
 import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
 
-export default function MedicineData(props) {
-    const [tableData, setTableData] = useState([]);
-    const columns = [
-        { field: "id", headerName: "Sr. No.", width: 100 },
-        { field: "name", headerName: "Name", width: 400 },
-        { field: "quantity", headerName: "Qty", width: 100 },
-        { field: "expiry", headerName: "Exp. Date", width: 130 },
-        {
-            field: "price",
-            headerName: "Price",
-            type: "number",
-            width: 90,
-        },
-    ];
+export default function MedicineDataTable(props) {
+  const [tableData, setTableData] = useState([]);
+  const columns = [
+    { field: "name", headerName: "Name", width: 400 },
+    { field: "quantity", headerName: "Qty", width: 100 },
+    { field: "expiry", headerName: "Exp. Date", width: 130 },
+    { field: "price", headerName: "Price", width: 90 },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 190,
+      renderCell: (params) => {
+        const deleteHandler = (event) => {};
+        return (
+          <>
+            <Button variant="contained" style={{ marginRight: "5px" }}>
+              Edit
+            </Button>
+            <Button variant="contained" onClick={deleteHandler}>
+              Delete
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
 
-    const onAddMedicine = () => {
-        const storageData = JSON.parse(localStorage.getItem("medicine"));
-        if (!storageData) {
-            return;
-        }
-        const data = storageData.map((item, i) => {
-            return {
-                id: i + 1,
-                name: item.name,
-                quantity: item.quantity,
-                price: item.price,
-                expiry: item.expiry,
-            };
-        });
-        setTableData(data);
-    };
-    useEffect(() => {
-        onAddMedicine();
-    }, []);
-    props.onAdd(onAddMedicine);
-    return (
-        <div style={{ height: 400, width: "100%" }}>
-            <DataGrid
-                rows={tableData}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-            />
-        </div>
-    );
+  const onAddMedicine = () => {
+    const storageData = JSON.parse(localStorage.getItem("medicine"));
+    if (!storageData) {
+      return;
+    }
+
+    setTableData(storageData);
+  };
+
+  useEffect(() => {
+    onAddMedicine();
+  }, []);
+
+  props.onAdd(onAddMedicine);
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={tableData}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
