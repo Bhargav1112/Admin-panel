6c180948f8bd37526c211f1e02935590765f5d9d
Bhakhar Bhargav <bhargavbhakhar1112@gmail.com>
2022-07-01 17:55:13 +0530

    patient table done

--- src/container/Paitentes/Patient.js
+++ src/container/Paitentes/Patient.js
@@ -7,120 +7,169 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import * as yup from "yup";
 import { useFormik } from "formik";
+import PatientData from "./PatientData";
 
 function Patient(props) {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    let renderDataFun;
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+        setIsEditing(false);
+        formik.resetForm();
+    };
 
-  const dataToLocalStorage = (values, action) => {
-    console.log(values);
-  };
+    const onAddData = (renderData) => {
+        renderDataFun = renderData;
+    };
 
-  const schema = yup.object().shape({
-    name: yup.string().required("Required"),
-    phone: yup.number().max(10, "Max 10 digits allowed").required("Required"),
-    address: yup.string().required("Required"),
-    ward: yup.string().required("Required"),
-  });
+    const dataToLocalStorage = (values, action) => {
+        const localData = JSON.parse(localStorage.getItem("patient"));
+        const id = Math.trunc(Math.random() * 10000 + 1);
+        const finalData = {
+            id,
+            ...values,
+        };
+        if (!localData) {
+            localStorage.setItem("patient", JSON.stringify([finalData]));
+        } else {
+            localData.push(finalData);
+            localStorage.setItem("patient", JSON.stringify(localData));
+        }
+        setOpen(false);
+        renderDataFun();
+        action.resetForm();
+    };
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      phone: "",
-      address: "",
-      ward: "",
-    },
-    validationSchema: schema,
-    onSubmit: dataToLocalStorage,
-  });
+    const updateHandler = (values, action) => {
+        const localData = JSON.parse(localStorage.getItem("patient"));
+        const existingItemIndex = localData.findIndex(
+            (data) => data.id === values.id
+        );
+        localData[existingItemIndex] = { ...values };
+        localStorage.setItem("patient", JSON.stringify(localData));
+        setOpen(false);
+        renderDataFun();
+        action.resetForm();
+    };
 
-  const { handleBlur, handleChange, handleSubmit, values, touched, errors } =
-    formik;
+    const schema = yup.object().shape({
+        name: yup.string().required("Required"),
+        phone: yup
+            .string()
+            .max(10, "Max 10 digits allowed")
+            .required("Required"),
+        address: yup.string().required("Required"),
+        ward: yup.string().required("Required"),
+    });
 
-  return (
-    <>
-      <h1 style={{ marginBottom: "3rem" }}>PATIENT DATA</h1>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Add +
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add New Patient Information</DialogTitle>
-        <DialogContent>
-          <TextField
-            margin="dense"
-            id="name"
-            name="name"
-            label="Name"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.name}
-          />
-          {errors.name && touched.name && (
-            <p style={{ color: "red" }}>{errors.name}</p>
-          )}
-          <TextField
-            margin="dense"
-            id="phone"
-            name="phone"
-            label="Contact Number"
-            type="number"
-            fullWidth
-            variant="standard"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.phone}
-          />
-          {errors.phone && touched.phone && (
-            <p style={{ color: "red" }}>{errors.phone}</p>
-          )}
-          <TextField
-            margin="dense"
-            id="address"
-            name="address"
-            label="Address"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.address}
-          />
-          {errors.address && touched.address && (
-            <p style={{ color: "red" }}>{errors.address}</p>
-          )}
-          <TextField
-            margin="dense"
-            id="ward"
-            name="ward"
-            label="Ward"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.ward}
-          />
-          {errors.ward && touched.ward && (
-            <p style={{ color: "red" }}>{errors.ward}</p>
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Add</Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            phone: "",
+            address: "",
+            ward: "",
+        },
+        validationSchema: schema,
+        onSubmit: isEditing ? updateHandler : dataToLocalStorage,
+    });
+
+    const { handleBlur, handleChange, handleSubmit, values, touched, errors } =
+        formik;
+
+    return (
+        <>
+            <h1 style={{ marginBottom: "3rem" }}>PATIENT DATA</h1>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Add +
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add New Patient Information</DialogTitle>
+                <form onSubmit={handleSubmit}>
+                    <DialogContent>
+                        <TextField
+                            margin="dense"
+                            id="name"
+                            name="name"
+                            label="Name"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.name}
+                        />
+                        {errors.name && touched.name && (
+                            <p style={{ color: "red" }}>{errors.name}</p>
+                        )}
+                        <TextField
+                            margin="dense"
+                            id="phone"
+                            name="phone"
+                            label="Contact Number"
+                            type="number"
+                            fullWidth
+                            variant="standard"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.phone}
+                        />
+                        {errors.phone && touched.phone && (
+                            <p style={{ color: "red" }}>{errors.phone}</p>
+                        )}
+                        <TextField
+                            margin="dense"
+                            id="address"
+                            name="address"
+                            label="Address"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.address}
+                        />
+                        {errors.address && touched.address && (
+                            <p style={{ color: "red" }}>{errors.address}</p>
+                        )}
+                        <TextField
+                            margin="dense"
+                            id="ward"
+                            name="ward"
+                            label="Ward"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.ward}
+                        />
+                        {errors.ward && touched.ward && (
+                            <p style={{ color: "red" }}>{errors.ward}</p>
+                        )}
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button type="submit">
+                            {isEditing ? "Update" : "Add"}
+                        </Button>
+                    </DialogActions>
+                </form>
+            </Dialog>
+            {/* data-table */}
+            <PatientData
+                onAdd={onAddData}
+                onEdit={handleClickOpen}
+                isEditing={setIsEditing}
+                formik={formik}
+            />
+        </>
+    );
 }
 
 export default Patient;
--- src/container/Paitentes/PatientData.js
+++ src/container/Paitentes/PatientData.js
@@ -1,37 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
+export default function PatientData(props) {
+    const [data, setData] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [deleteId, setDeleteId] = useState(0);
 
-export default function DataTable() {
-  return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
-  );
+    const handleClickOpen = (params) => {
+        setOpen(true);
+        setDeleteId(params.id);
+        console.log(params.id);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const showData = () => {
+        const localData = JSON.parse(localStorage.getItem("patient"));
+        if (!localData) return;
+        setData(localData);
+    };
+
+    const deleteHandler = () => {
+        const localData = JSON.parse(localStorage.getItem("patient"));
+        const updatedData = localData.filter(
+            (patient) => patient.id !== deleteId
+        );
+        localStorage.setItem("patient", JSON.stringify(updatedData));
+        setOpen(false);
+        showData();
+    };
+
+    const editHandler = (params) => {
+        props.onEdit();
+        props.isEditing(true);
+        props.formik.setValues(params.row);
+    };
+
+    useEffect(() => {
+        showData();
+    }, []);
+
+    props.onAdd(showData);
+
+    const columns = [
+        { field: "name", headerName: "Name", width: 270 },
+        { field: "phone", headerName: "Contact No.", width: 150 },
+        { field: "address", headerName: "Address", width: 300 },
+        { field: "ward", headerName: "Ward", width: 100 },
+        {
+            field: "action",
+            headerName: "Actions",
+            width: 100,
+            renderCell: (params) => {
+                return (
+                    <>
+                        <IconButton
+                            aria-label="edit"
+                            onClick={editHandler.bind(null, params)}
+                        >
+                            <EditIcon />
+                        </IconButton>
+                        <IconButton
+                            aria-label="delete"
+                            onClick={handleClickOpen.bind(null, params)}
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                    </>
+                );
+            },
+        },
+    ];
+    return (
+        <>
+            <div style={{ height: 400, width: "100%" }}>
+                <DataGrid
+                    rows={data}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
+            </div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">Are you sure?</DialogTitle>
+                <DialogActions>
+                    <Button onClick={handleClose}>No</Button>
+                    <Button onClick={deleteHandler} autoFocus>
+                        Yes
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
 }
