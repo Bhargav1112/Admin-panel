8c68d547f661e4cb13d09a6bae30b21765c2539d
Bhargav1112 <bhargavbhakhar1112@gmail.com>
2022-07-21 15:31:07 +0530

    json-server

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./index";
+// import thunk from "redux-thunk";
+// import rootReducer from "./index";
+import counterReducer from "./reducer/counterReducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: { count: counterReducer },
+});
 
 export default store;
